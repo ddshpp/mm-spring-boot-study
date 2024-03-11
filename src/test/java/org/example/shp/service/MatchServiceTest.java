69285c6df3ea35f7f6a90763122ebf0bcc39b0ba
ddshpp <dshp753@gmail.com>
2024-03-11 22:46:43 +0900

    test: test 추가

--- src/test/java/org/example/shp/service/MatchServiceTest.java
+++ src/test/java/org/example/shp/service/MatchServiceTest.java
@@ -1,8 +1,25 @@
 package org.example.shp.service;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class MatchServiceTest {
 
+    @Test()
+    @DisplayName("첫번째 테스트")
+    public void test() {
+        // given
+        long a = 10;
+        long expected = 10;
+
+        // when
+
+        // then
+        assertThat(a).isEqualTo(expected);
+    }
 }
